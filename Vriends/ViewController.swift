2fb74f3cd7dc8930b176dc021fe24e2602ac5b3c
Tanner Luke <TannerLuke@macbook-pro.local>
2018-09-09 16:29:38 -0500

    Initial Commit

---
+++ Vriends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vriends
+//
+//  Created by Tanner Luke on 9/9/18.
+//  Copyright © 2018 Tanner Luke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
